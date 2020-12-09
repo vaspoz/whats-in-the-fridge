f2586d395776f5c98e23e83796782c3410f1331e
vaspoz <vaspoz@mail.ru>
2020-12-09 16:57:09 +0100

    init temp views and serializers

--- witf/whatisinthefridge/serializers.py
+++ witf/whatisinthefridge/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
 
-class UserSerialized(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
